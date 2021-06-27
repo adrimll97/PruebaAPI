d317e89337a788fb79fbf6b9d9de229a6f64efbf
Adrian <adrigalileo@gmail.com>
2021-06-27 12:32:59 +0200

    fix an error creating comunicados whitout comunicado_anterior (#6)

--- app/models/comunicado.rb
+++ app/models/comunicado.rb
@@ -1,7 +1,7 @@
 class Comunicado < ApplicationRecord
   belongs_to :creador, class_name: "Persona"
   belongs_to :receptor, class_name: "Persona"
-  belongs_to :comunicado_anterior, class_name: "Comunicado"
+  belongs_to :comunicado_anterior, class_name: "Comunicado", optional: true
 
   has_many :adjuntos, foreign_key: "comunicado_id"
 end
